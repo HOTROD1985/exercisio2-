c3c7202c4b6ae1d0adfe9669bb5992d736f6ccc9
HOTROD1985 <pietro.ferrari@sounotredame.org.br>
2024-10-28 08:17:55 -0300

    exer

---
+++ script.js
@@ -0,0 +1,9 @@
+let numeros = [2, 5, 8, 13, 16, 21];
+ 
+numeros.forEach(function (PegaNumerosIpares){
+if(PegaNumerosIpares % 2 === 0){
+    numerosimpares.push(PegaNumerosIpares)
+    numerosimpares* Math.round(Math.random* 10)
+}
+ })
+ console.log (numerosimpares);
